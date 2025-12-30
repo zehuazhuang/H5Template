40c9eedd06fec9ba0087730edbfa76598c7e5d22
liuhoujun <168155160+zehuazhuang@users.noreply.github.com>
2025-12-30 14:55:54 +0800

    改充值key、

--- mock/modules/coins.mock.ts
+++ mock/modules/coins.mock.ts
@@ -1,52 +1,52 @@
 // 金币选项数据
 export default [
   {
-    key: 'lvbsvhxcgcrvesor',
+    key: 'wtjwtjpbltoadkay',
     cions: 400,
     meney: 0.99
   },
   {
-    key: '',
+    key: 'alriozqkjpashpjd',
     cions: 800,
     meney: 1.99
   },
   {
-    key: '',
+    key: 'jtpawdkblyotjtw',
     cions: 2190,
     meney: 3.99
   },
   {
-    key: 'dxismgcwewhrtezo',
+    key: 'oyquxixdbvrdevee',
     cions: 2450,
     meney: 4.99
   },
   {
-    key: '',
+    key: 'lbtdwkjyapttowj',
     cions: 3950,
     meney: 8.99
   },
   {
-    key: '',
+    key: 'cpmofkshxjibcoxr',
     cions: 4900,
     meney: 9.99
   },
     {
-    key: '',
+    key: 'twjybkpldatowjt',
     cions: 5700,
     meney: 13.99
   },
     {
-    key: '',
+    key: 'siwrcpcyptnbbnnk',
     cions: 9800,
     meney: 19.99
   },
     {
-    key: '',
+    key: 'iousphcwlfnerxvi',
     cions: 24500,
     meney: 49.99
   },
     {
-    key: '',
+    key: 'rjtxewbnhsxawygv',
     cions: 49000,
     meney: 99.99
   },
